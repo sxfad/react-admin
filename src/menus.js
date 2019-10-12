9c069c5a8ab7156a34120a41154a1590ac7d4129
zkboys <876213774@qq.com>
2019-10-12 10:48:11 +0800

    调整菜单

--- src/menus.js
+++ src/menus.js
@@ -1,4 +1,3 @@
-import antdMenus from './menus-ant-design-example';
 /*
 * 菜单数据 返回Promise各式，支持前端硬编码、异步获取菜单数据
 * */
@@ -25,12 +24,11 @@ export default function getMenus(userId) {
         {key: 'document', text: '文档', icon: 'book', url: 'https://open.vbill.cn/react-admin', target: '_blank', order: 1200},
 
         {key: 'menus', text: '菜单编辑', icon: 'lock', path: '/menu-permission', order: 1000},
-        {key: 'codeGenerator', text: '代码生成', icon: 'code', path: '/admin-crud', order: 999},
         {key: 'ajax', text: 'ajax请求', icon: 'api', path: '/example/ajax', order: 998},
         {key: 'user', text: '用户列表', icon: 'user', path: '/users', order: 900},
         {key: 'role', text: '角色列表 ', icon: 'team', path: '/roles', order: 800},
         {key: 'page404', text: '404页面不存', icon: 'file-search', path: '/404', order: 700},
         {key: 'user-center', text: '用户中心', icon: 'user', path: '/user-center', order: 600},
         {key: 'component', text: '组件', icon: 'ant-design', order: 700},
-    ].concat(antdMenus));
+    ]);
 }
