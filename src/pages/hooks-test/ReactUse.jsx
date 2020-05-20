65c89357a01d614eac4c9d323c489ca81201481e
zkboys <876213774@qq.com>
2020-05-20 10:43:43 +0800

    hooks

---
+++ src/pages/hooks-test/ReactUse.jsx
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {useEffectOnce, useLifecycles, useMount, useUnmount, useMap} from 'react-use';
+import {Button} from 'antd';
+import config from 'src/commons/config-hoc';
+import PageContent from 'src/layouts/page-content';
+
+export default config({
+    path: '/react-use',
+    title: 'React Use',
+})(props => {
+    // 每次组件更新，函数都会被调用一次，如果初始化相关数据，写到 useEffect didMount中
+    console.log('react-use');
+
+    useMount(() => {
+        console.log('useMount');
+    });
+
+    useUnmount(() => {
+        console.log('useUnmount');
+    });
+
+    useEffectOnce(() => {
+        // componentDidMount
+        console.log('Running effect once on mount');
+
+        return () => {
+            // componentWillUnmount
+            console.log('Running clean-up of effect on unmount');
+        };
+    });
+
+    useLifecycles(() => {
+        // componentDidMount
+        console.log('MOUNTED');
+    }, () => {
+        // componentWillUnmount
+        console.log('UNMOUNTED');
+    });
+
+    let [count, setCount] = useState(0);
+    const [state, stater] = useMap({
+        hello: 'there',
+    });
+
+
+    return (
+        <PageContent>
+            hooks page
+            <div>
+                <button onClick={() => stater.set(String(Date.now()), new Date().toJSON())}>
+                    Add
+                </button>
+                <button onClick={() => stater.reset()}>
+                    Reset
+                </button>
+                <button onClick={() => stater.set('hello', '123')}>
+                    Set new data
+                </button>
+                <button onClick={() => {
+                    state.age = 23;
+                    stater.setAll({...state});
+                }}>
+                    Set new data and clear others
+                </button>
+                <button onClick={() => stater.remove('hello')} disabled={!state.hello}>
+                    Remove 'hello'
+                </button>
+                <pre>{JSON.stringify(state, null, 2)}</pre>
+            </div>
+            <div>
+                count: {count}
+                <Button onClick={() => setCount(--count)}>-</Button>
+                <Button onClick={() => setCount(++count)}>+</Button>
+            </div>
+        </PageContent>
+    );
+});
--- src/pages/hooks-test/index.jsx
+++ src/pages/hooks-test/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Button} from 'antd';
 import config from 'src/commons/config-hoc';
 import PageContent from 'src/layouts/page-content';
 
@@ -6,10 +7,38 @@ export default config({
     path: '/hooks-test',
     title: 'config 函数式 用法',
 })(props => {
+    // 每次组件更新，函数都会被调用一次，如果初始化相关数据，写到 useEffect didMount中
+
+    let a = 123;
     console.log(props);
+    let [count, setCount] = useState(0);
+
+    // componentDidMount
+    useEffect(() => {
+        console.log(123);
+
+        // componentWillUnmount
+        return () => {
+            console.log('unmount1');
+        };
+    }, []);
+
+    // componentWillUnmount
+    useEffect(() => () => console.log('unmount2'));
+
+    // componentDidMount or componentDidUpdate
+    useEffect(() => console.log('mounted or updated'));
+
     return (
         <PageContent>
             hooks page
+
+            <div>a : {a}</div>
+            <div>
+                count: {count}
+                <Button onClick={() => setCount(--count)}>-</Button>
+                <Button onClick={() => setCount(++count)}>+</Button>
+            </div>
         </PageContent>
     );
 });
