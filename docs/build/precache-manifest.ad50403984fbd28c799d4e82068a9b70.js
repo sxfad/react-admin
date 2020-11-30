a02d58557da8417c0cde68f55d6ac1c66f1529ce
zkboys <876213774@qq.com>
2020-11-30 11:04:13 +0800

    :pencil: 整理文档

---
+++ docs/build/precache-manifest.ad50403984fbd28c799d4e82068a9b70.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a4551991b6d4c4d9734c40b999cdc9",
+    "url": "/react-admin/build/index.html"
+  },
+  {
+    "revision": "3c1734c25288e866d234",
+    "url": "/react-admin/build/static/css/10.e52b1923.chunk.css"
+  },
+  {
+    "revision": "47f79f374d69eeb7a6ed",
+    "url": "/react-admin/build/static/css/11.ef9b3fec.chunk.css"
+  },
+  {
+    "revision": "27bc6465c13380fda32f",
+    "url": "/react-admin/build/static/css/13.cd9bebb0.chunk.css"
+  },
+  {
+    "revision": "54d9beebbb7e0147263d",
+    "url": "/react-admin/build/static/css/14.acc0c57d.chunk.css"
+  },
+  {
+    "revision": "ecb68ada9154370610b7",
+    "url": "/react-admin/build/static/css/15.26ba442e.chunk.css"
+  },
+  {
+    "revision": "1c9125373e303b40eb1f",
+    "url": "/react-admin/build/static/css/2.0e5fe661.chunk.css"
+  },
+  {
+    "revision": "5c4d2a329cf1c4127805",
+    "url": "/react-admin/build/static/css/3.48bf13c9.chunk.css"
+  },
+  {
+    "revision": "3f35334a2aaea6a9d670",
+    "url": "/react-admin/build/static/css/4.8e9afd67.chunk.css"
+  },
+  {
+    "revision": "a4330908629174e8dd47",
+    "url": "/react-admin/build/static/css/5.5117689a.chunk.css"
+  },
+  {
+    "revision": "e1c436027afd5d187d34",
+    "url": "/react-admin/build/static/css/6.22df6a94.chunk.css"
+  },
+  {
+    "revision": "69869b035f470b30b824",
+    "url": "/react-admin/build/static/css/main.3811ac11.chunk.css"
+  },
+  {
+    "revision": "3c1734c25288e866d234",
+    "url": "/react-admin/build/static/js/10.58a8a03c.chunk.js"
+  },
+  {
+    "revision": "47f79f374d69eeb7a6ed",
+    "url": "/react-admin/build/static/js/11.b3c770c5.chunk.js"
+  },
+  {
+    "revision": "957ca20f687d0ca75b8b",
+    "url": "/react-admin/build/static/js/12.4b663524.chunk.js"
+  },
+  {
+    "revision": "27bc6465c13380fda32f",
+    "url": "/react-admin/build/static/js/13.e030edc8.chunk.js"
+  },
+  {
+    "revision": "54d9beebbb7e0147263d",
+    "url": "/react-admin/build/static/js/14.3cbf6140.chunk.js"
+  },
+  {
+    "revision": "ecb68ada9154370610b7",
+    "url": "/react-admin/build/static/js/15.e85a7f81.chunk.js"
+  },
+  {
+    "revision": "d6a704b6a9e8f455b8b2",
+    "url": "/react-admin/build/static/js/16.834c635c.chunk.js"
+  },
+  {
+    "revision": "4cc1e5232458b1917d50",
+    "url": "/react-admin/build/static/js/17.82ef266b.chunk.js"
+  },
+  {
+    "revision": "8e960a7904398b5470ac",
+    "url": "/react-admin/build/static/js/18.2029092d.chunk.js"
+  },
+  {
+    "revision": "fedb21b738a915d65084",
+    "url": "/react-admin/build/static/js/19.627e797c.chunk.js"
+  },
+  {
+    "revision": "1c9125373e303b40eb1f",
+    "url": "/react-admin/build/static/js/2.cb2a63c1.chunk.js"
+  },
+  {
+    "revision": "175b6492ba8d5fa0fecd",
+    "url": "/react-admin/build/static/js/20.7a6ca9a5.chunk.js"
+  },
+  {
+    "revision": "af7ad468b6b686ec787d",
+    "url": "/react-admin/build/static/js/21.4aff8b4e.chunk.js"
+  },
+  {
+    "revision": "2e7afa8061436e81fcdc",
+    "url": "/react-admin/build/static/js/22.c0b44b6d.chunk.js"
+  },
+  {
+    "revision": "f6ebef5b0c9814d49534",
+    "url": "/react-admin/build/static/js/23.6685a99a.chunk.js"
+  },
+  {
+    "revision": "3ff04992ce7bfc59a414",
+    "url": "/react-admin/build/static/js/24.635a49a5.chunk.js"
+  },
+  {
+    "revision": "4206864af7892ec65529",
+    "url": "/react-admin/build/static/js/25.6388233a.chunk.js"
+  },
+  {
+    "revision": "5c4d2a329cf1c4127805",
+    "url": "/react-admin/build/static/js/3.88158476.chunk.js"
+  },
+  {
+    "revision": "78d1fd02c85de9f8f29a8830ca0aa750",
+    "url": "/react-admin/build/static/js/3.88158476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f35334a2aaea6a9d670",
+    "url": "/react-admin/build/static/js/4.734ed774.chunk.js"
+  },
+  {
+    "revision": "a4330908629174e8dd47",
+    "url": "/react-admin/build/static/js/5.35627b5d.chunk.js"
+  },
+  {
+    "revision": "e1c436027afd5d187d34",
+    "url": "/react-admin/build/static/js/6.952dbe24.chunk.js"
+  },
+  {
+    "revision": "1ec12a7daa4ee735561a",
+    "url": "/react-admin/build/static/js/7.18ec7c52.chunk.js"
+  },
+  {
+    "revision": "643b3fb4959cbc6fe125",
+    "url": "/react-admin/build/static/js/8.f7c0e9a6.chunk.js"
+  },
+  {
+    "revision": "2fc22b5a75a13c8ae5ec",
+    "url": "/react-admin/build/static/js/9.bc0d29f6.chunk.js"
+  },
+  {
+    "revision": "69869b035f470b30b824",
+    "url": "/react-admin/build/static/js/main.cd89c00e.chunk.js"
+  },
+  {
+    "revision": "2b2e429c617e91265a5d5c6722892f11",
+    "url": "/react-admin/build/static/js/reactVendor_45666092243205e01b0c.dll.js"
+  },
+  {
+    "revision": "a80de09429f686964feb",
+    "url": "/react-admin/build/static/js/runtime-main.7eda95ad.js"
+  },
+  {
+    "revision": "874d703eeed00b19cd08097933b85520",
+    "url": "/react-admin/build/static/js/vendor_45666092243205e01b0c.dll.js"
+  },
+  {
+    "revision": "095640df1cfa09b2278b74bd2b95fcf5",
+    "url": "/react-admin/build/static/media/404.095640df.png"
+  },
+  {
+    "revision": "86b1fa70577f42141ec50bb75015a04c",
+    "url": "/react-admin/build/static/media/bg.86b1fa70.png"
+  },
+  {
+    "revision": "3d2fc6ca7fe059959639beba73f65256",
+    "url": "/react-admin/build/static/media/login-bg.3d2fc6ca.jpg"
+  },
+  {
+    "revision": "0454d3a7f4f7cc6c85c49c079fad8398",
+    "url": "/react-admin/build/static/media/star.0454d3a7.png"
+  }
+]);
--- docs/build/service-worker.js
+++ docs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.162c142352bab166dfa0066a83023063.js"
+  "/react-admin/build/precache-manifest.ad50403984fbd28c799d4e82068a9b70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-admin/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
