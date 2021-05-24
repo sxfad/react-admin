03956196af532125bb1df6be1094c861a1924b5e
zkboys <876213774@qq.com>
2021-05-24 14:09:20 +0800

    :pencil: 整理文档

--- docs/build/static/js/20.8e756351.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpant-admin"]=this["webpackJsonpant-admin"]||[]).push([[20],{531:function(n,e,t){"use strict";t.r(e);var a=t(105),c=t(555),i=t(7);e.default=Object(a.a)({path:"/demo/config/hoc/:id",header:!1,side:!1})((function(n){return console.log(99,n),Object(i.jsx)(c.a,{children:"config \u9ad8\u9636\u7ec4\u4ef6"})}))}}]);
-//# sourceMappingURL=20.8e756351.chunk.js.map
