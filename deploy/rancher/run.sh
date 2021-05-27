97b5b2750df935e428913c4edc50473593afcb15
zkboys <876213774@qq.com>
2021-05-27 16:36:53 +0800

    :sparkles: 构建

--- deploy/rancher/run.sh
+++ deploy/rancher/run.sh
@@ -12,12 +12,12 @@ cd deploy/rancher || exit
 
 # 构建docker镜像
 cat Dockerfile
-docker build --no-cache -t bh-harbor.suixingpay.com/zhaikun/${JOB_BASE_NAME}-${APP_ENV}:${BUILD_ID} -f Dockerfile .
-docker push bh-harbor.suixingpay.com/zhaikun/${JOB_BASE_NAME}-${APP_ENV}:${BUILD_ID}
+docker build --no-cache -t bh-harbor.suixingpay.com/zhaikun/${JOB_BASE_NAME}:${BUILD_ID} -f Dockerfile .
+docker push bh-harbor.suixingpay.com/zhaikun/${JOB_BASE_NAME}:${BUILD_ID}
 
-# rancher发布
-sed -i "s/JOB_BASE_NAME/${JOB_BASE_NAME}-${APP_ENV}/g" deployment.yaml
-sed -i "s/BUILD_ID/${BUILD_ID}/g" deployment.yaml
+# rancher发布  TODO 注意修改 NAMESPACE_NAME
 sed -i "s/NAMESPACE_NAME/front-center/g" deployment.yaml
+sed -i "s/JOB_BASE_NAME/${JOB_BASE_NAME}/g" deployment.yaml
+sed -i "s/BUILD_ID/${BUILD_ID}/g" deployment.yaml
 cat deployment.yaml
 kubectl  apply -f deployment.yaml
