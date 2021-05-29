8e03f8e35745d2bc047519fa687a4c6b9f13f34e
zkboys <876213774@qq.com>
2021-05-29 08:45:58 +0800

    :ambulance: 修改部署目录

--- deploy/jenkins.sh
+++ deploy/jenkins.sh
@@ -4,18 +4,18 @@ yarn
 # 前端构建
 yarn build
 
-# 将构建生成的静态文件copy到rancher目录，提升docker构建速度
-rm -rf rancher/build && cp -r build/ rancher/build
+# 将构建生成的静态文件copy到deploy目录，提升docker构建速度
+rm -rf deploy/build && cp -r build/ deploy/build
 
-# 进入rancher目录
-cd rancher
+# 进入deploy目录
+cd deploy
 
 # 构建docker镜像
 cat Dockerfile
 docker build --no-cache -t bh-harbor.suixingpay.com/zhaikun/${JOB_BASE_NAME}:${BUILD_ID} -f Dockerfile .
 docker push bh-harbor.suixingpay.com/zhaikun/${JOB_BASE_NAME}:${BUILD_ID}
 
-# rancher发布
+# deploy发布
 # TODO 注意修改 NAMESPACE_NAME
 sed -i "s/NAMESPACE_NAME/front-center/g" deployment.yaml
 sed -i "s/JOB_BASE_NAME/${JOB_BASE_NAME}/g" deployment.yaml
