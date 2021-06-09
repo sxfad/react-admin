c7c616d2c098c9677d215bd80b4201613c4ebd6c
zkboys <876213774@qq.com>
2021-06-09 10:44:34 +0800

    :art: 整理代码

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,20 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/api',
         {
-            // target: 'http://172.16.41.92:8080', // 孔健
-            target: 'http://172.16.178.69:8080', // 测试
+            target: 'http://172.16.40.72:8080',
             pathRewrite: {
                 '^/api': '', // 如果后端接口无前缀，可以通过这种方式去掉
             },
             changeOrigin: true,
             secure: false, // 是否验证证书
             ws: true, // 启用websocket
+            // onProxyReq (proxyReq, req, res) {
+            // },
+            onProxyRes(proxyRes, req, res) {
+                proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+                proxyRes.headers['Access-Control-Allow-Methods'] = '*';
+                proxyRes.headers['Access-Control-Allow-Headers'] = '*';
+            },
         },
     ));
 };
