ee323ad5f26ae45b3388c1953720945c13615938
zkboys <876213774@qq.com>
2021-07-06 15:26:43 +0800

    :art: 整理代码

---
+++ docs/build/static/js/12.de004858.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp_react-admin"]=this["webpackJsonp_react-admin"]||[]).push([[12],{1044:function(t,e,c){"use strict";c.r(e);c(1088);var s=c(1115),j=(c(417),c(73)),i=(c(1090),c(1114)),a=c(14),n=c(181),r=c(10);e.default=Object(n.a)({path:"/test"})((function(t){return Object(r.jsxs)(a.m,{children:[Object(r.jsx)("div",{children:Object(r.jsx)(i.a,{})}),Object(r.jsx)("div",{children:Object(r.jsx)(j.a,{})}),Object(r.jsx)("div",{children:Object(r.jsx)(s.a,{})})]})}))}}]);
+//# sourceMappingURL=12.de004858.chunk.js.map
