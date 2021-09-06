388402a82dfe8cc85cfb9fb65e7e22ddccb575f1
zkboys <876213774@qq.com>
2021-09-06 16:27:36 +0800

    :art: 整理代码

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,7 +11,6 @@ export default config({
 
     // 如果其他页面作为首页，直接重定向，config中不要设置title，否则tab页中会多个首页
     // return <Redirect to="/users"/>;
-
     return (
         <PageContent className={styles.root}>
             <h1>首页</h1>
